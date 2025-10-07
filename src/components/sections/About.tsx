894323399ef6974b2a9f1d2f1a1160d760830a8c
rahulio96 <rahulgithub96@gmail.com>
2025-10-06 20:59:46 -0700

    Update About section

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -11,8 +11,8 @@ const About = () => {
         viewport={{ once: true, amount: 0.3 }}
         transition={{ duration: 1 }}
       >
-        <p>I’m a computer science student at CSU Sacramento, graduating in December 2025, with hands-on experience building real-world software across startups, non-profits, and open-source communities.</p>
-        <p>As a Software Engineer Intern at Intel, I'm renewing automation efforts and debugging platform issues. Previously, I built key mobile app features as a Frontend Developer at an early-stage startup.</p>
+        <p>I’m a computer science student at CSU Sacramento, graduating in December 2025, with hands-on experience building real-world software across startups, non-profits, and open-source projects.</p>
+        <p>As a Debug Software Engineer Intern at Intel, I'm renewing automation efforts and debugging platform issues. Previously, I built key mobile app features as a Frontend Developer at an early-stage startup.</p>
         <p>I’ve contributed to open-source tools like npm (used by 17M+ developers), and led teams in delivering full-stack apps, including a chemical inventory manager mobile app for a client and an AI-powered education tool built during a hackathon.</p>
         <p>I’m passionate about learning, collaborating with talented people, and building impactful software that serves real users.</p>
       </motion.div>
